6dd4a935afc20890e08cd16dab1b2a24b2d49f10
alexabdrustefu <alessandro.sstefu06@gmail.com>
2024-06-27 16:55:58 +0200

    push

--- src/app/Features/dashboard/dashboard.component.ts
+++ src/app/Features/dashboard/dashboard.component.ts
@@ -1,11 +1,23 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../Service/auth/auth.service";
 import {AreaService} from "../../Service/area.service";
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatGridList} from "@angular/material/grid-list";
+import {MatIcon} from "@angular/material/icon";
+import {FlexModule} from "@angular/flex-layout";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatGridList,
+    MatIcon,
+    MatCardTitle,
+    FlexModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
